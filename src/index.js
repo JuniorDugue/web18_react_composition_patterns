e3eebe9943a9d77cb031f30e9fb199066778efd2
Junior Dugue <jrdugue@gmail.com>
2019-04-15 23:49:12 -0400

    practicing higher order functions
    practicing higher order functions

--- src/index.js
+++ src/index.js
@@ -6,11 +6,33 @@ import "./styles.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Title text="Hello CodeSandBox" />
+      <SubTitle text="Start editing to see some magic happen!" />
     </div>
   );
 }
+
+const Title = props => {
+  const upperCaseText = props.text.toUpperCase();
+  return <h1>{upperCaseText}</h1>;
+};
+
+const SubTitle = props => {
+  const upperCaseText = props.text.toUpperCase();
+  return <h2>{upperCaseText}</h2>;
+};
+
+//write an HOC that turns a components prop to all caps
+const Caps = Component => props => {
+  const upperCaseText = props.text.toUpperCase();
+  return <Component />;
+};
+
+const AngryTitle = Caps(Title);
+
+const AngrySubtitle = Caps(SubTitle);
+
+Caps(Title); // ==> ?? an anonymous functional comp
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
